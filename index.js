d7ee232c788c60c1413e38c22467f9a9223d105d
Leo Wilkin <leo@wilkin.xyz>
2025-07-10 20:44:57 -0400

    Add show alias function for alias information

--- index.js
+++ index.js
@@ -6,23 +6,16 @@ import { z } from "zod";
 
 const server = new McpServer({
     name: "mcp-migadu",
-    version: "2.2.0"
+    version: "2.2.1"
 });
 
-// this MCP at the moment lets you add a new alias to an existing inbox :)
-
-const inputSchema = {
-    alias: z.string().describe("The raw inbox name part of the alias. e.g if the alias is hello@example.com, enter hello."),
-    domain: z.string().describe("The domain of the email address. e.g. example.com"),
-    forwarding_address: z.string().describe("The existing email address that the alias should forward to. Can only be on the same domain. e.g. hello@example.com as the alias can only forward to leo@example.com")
-}
-
-async function createAlias({ alias, domain, forwarding_address }) {
+async function createAlias({ alias, domain, forwarding_address, is_internal }) {
     const url = `https://api.migadu.com/v1/domains/${domain}/aliases`;
     const auth = Buffer.from(`${process.env.MIGADU_USER}:${process.env.MIGADU_API_KEY}`).toString('base64');
     const body = JSON.stringify ({
         local_part: alias,
-        destinations: [forwarding_address]
+        destinations: [forwarding_address],
+        is_internal
     });
 
 
@@ -46,11 +39,49 @@ async function createAlias({ alias, domain, forwarding_address }) {
    };
 }
 
+async function showAlias({ alias, domain }) {
+    const url = `https://api.migadu.com/v1/domains/${domain}/aliases/${alias}`;
+    const auth = Buffer.from(`${process.env.MIGADU_USER}:${process.env.MIGADU_API_KEY}`).toString('base64');
+
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Basic ${auth}`,
+            'Content-Type': 'application/json'
+        },
+    });
+
+    if (!response.ok) {
+    throw new Error(`Couldn't show existing alias:`, response.body);
+   }
+
+   const data = await response.json();
+
+   return {
+        content: [{ type: "text", text: JSON.stringify(data) }]
+   };
+}
+
 server.registerTool("create_alias", {
     title: "Create a new email alias",
     description: "To create a new alias demo@mydomain.org with destinations one@mydomain.org and two@mydomain.org you can call the aliases API as in the example below. Note that aliases can redirect only on the same domain.",
-    inputSchema,
+    inputSchema: {
+        alias: z.string().describe("The raw inbox name part of the alias. e.g if the alias is hello@example.com, enter hello."),
+        domain: z.string().describe("The domain of the email address. e.g. example.com"),
+        forwarding_address: z.string().describe("The existing email address that the alias should forward to. Can only be on the same domain. e.g. hello@example.com as the alias can only forward to leo@example.com"),
+        is_internal: z.boolean().optional().describe("If you want setup internal, private alias and restrict it to receive messages only via Migadu outgoing servers, please set option is_internal to `true` like in the example below. Note that no external messages would be accepted.").default(false)
+    },
 }, createAlias);
+
+server.registerTool("show_alias", {
+    title: "Show an existing alias",
+    description: "Individual aliases are accessible by their local part for individual operations. For example, to show alias information of demo@mydomain.org , we could call the API as in the example below.",
+    inputSchema: {
+        alias: z.string().describe("The raw inbox name part of the alias. e.g if the alias is hello@example.com, enter hello."),
+        domain: z.string().describe("The domain of the email address. e.g. example.com"),
+    },
+}, showAlias);
 
 const transport = new StdioServerTransport();
 await server.connect(transport)
